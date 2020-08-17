eadb144d96941b9f0bf4762a9a02adaffeb32677
Junior D Perdomo T <jdaperdomo@falabella.cl>
2020-08-16 21:18:23 -0400

    initial commit

---
+++ bot.js
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const axios = require('axios');
+const { Telegraf } = require('telegraf');
+const bot = new Telegraf('1226295178:AAHYjBb6I-AMpfSw3CG3GtHeDOEV6l7w3Zk')
+let language
+
+bot.start((ctx) => ctx.reply('Welcome'))
+
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+
+bot.command('lang', (ctx) => {
+    const lang = ctx.message.text.split(" ")
+    if(lang[1] === 'ES'){
+        language = 'spanish'
+    }
+
+    ctx.reply(`You have selected ${language}`)
+})
+
+bot.use(async (ctx) => {
+    let data = {}
+    await axios.get(`https://manager-english-bot.herokuapp.com/word/${ctx.message.text}`)
+        .then(res => {
+            data = res.data
+        })
+        .catch(err => {
+            console.log(err)
+        });
+    if (data.pronunciations) {
+        ctx.replyWithAudio({
+            url: data.pronunciations
+        }, {
+            title: ctx.message.text,
+            caption: data.definitions
+        })
+    } else {
+        ctx.reply(`${data} 👎`)
+    }
+})
+bot.launch()
